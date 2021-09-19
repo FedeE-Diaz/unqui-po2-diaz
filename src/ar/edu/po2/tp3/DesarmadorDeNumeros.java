1fa8878ca0e252765e8efd3222aeac04fd7d9379
Federico Díaz <fedee_diaz@hotmail.com>
2021-09-19 19:50:08 -0300

    DesarmadoDeNumeros Sin terminar

---
+++ src/ar/edu/po2/tp3/DesarmadorDeNumeros.java
@@ -0,0 +1,9 @@
+package ar.edu.po2.tp3;
+
+public class DesarmadorDeNumeros {
+	int[] arrayDeEnteros;
+	
+	DesarmadorDeNumeros(int [] unArrayDeEnteros){
+		this.arrayDeEnteros = unArrayDeEnteros;
+	}
+}
