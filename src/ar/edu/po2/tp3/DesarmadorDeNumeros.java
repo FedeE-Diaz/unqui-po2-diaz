8befa7e9a6b4a46dc36e27ad88217cbb7bf87dcc
Federico Díaz <fedee_diaz@hotmail.com>
2021-09-19 19:50:32 -0300

    DesarmadorDeNumeros sin terminar

--- src/ar/edu/po2/tp3/DesarmadorDeNumeros.java
+++ src/ar/edu/po2/tp3/DesarmadorDeNumeros.java
@@ -1,9 +1,32 @@
 package ar.edu.po2.tp3;
 
+import java.util.ArrayList;
+
 public class DesarmadorDeNumeros {
-	int[] arrayDeEnteros;
 	
-	DesarmadorDeNumeros(int [] unArrayDeEnteros){
-		this.arrayDeEnteros = unArrayDeEnteros;
+	
+	public int selectorDeEnterosConMayorCantidadDePares(int[] arrayDeEnteros) {
+	
+		ArrayList arrayDeCantidadDePares = new ArrayList();
+	
+		for(int numero: arrayDeEnteros) {
+			Counter contador = new Counter(this.numeroDesarmado(numero));
+			arrayDeCantidadDePares.add(contador.cantidadPares())
+		}
+	
+	}
+	
+	private ArrayList numeroDesarmado(int numeroEntero) {
+		ArrayList arrayDeDigitos = new ArrayList();
+		double restoDeNumero = numeroEntero;
+		int baseDecimalAplicable = this.cantidadDeDigitos(numeroEntero);
+		
+		for (int i = 0; i == this.cantidadDeDigitos(numeroEntero); i++){
+			arrayDeDigitos.add(this.digitoDePosicion(restoDeNumero, baseDecimalAplicable)); 
+			restoDeNumero = restoDeNumero - Math.pow(10, baseDecimalAplicable);
+		}
+		
+		
+		return arrayDeDigitos;
 	}
 }
